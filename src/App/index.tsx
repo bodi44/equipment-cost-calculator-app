0175d6657c19109ca9bc016a8690494fc98645c3
bohdan-shtohrinets-sigma <bohdan.shtohrinets@emodoinc.com>
2024-03-10 19:26:58 +0200

    fix prod routing

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,17 +1,24 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
 import Cost from '../pages/Cost';
 import Speed from '../pages/Speed';
 import Error from '../pages/Error';
 
+const baseUrl = process.env.NODE_ENV === 'production' ? '/equipment-cost-calculator-app/' : '/';
+
 const router = createBrowserRouter([
-  { path: '/', errorElement: <Error /> },
   {
-    path: '/cost',
+    path: baseUrl,
+    element: <Navigate to={`${baseUrl}cost`}/>,
+    errorElement: <Error />,
+    children: [],
+  },
+  {
+    path: `${baseUrl}/cost`,
     element: <Cost />,
     errorElement: <Error />,
   },
   {
-    path: '/speed',
+    path: `${baseUrl}/speed`,
     element: <Speed />,
     errorElement: <Error />,
   },
