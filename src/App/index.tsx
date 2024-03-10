36383d4a2662d02ee80d78597c74bcd9704d5ca3
bohdan-shtohrinets-sigma <bohdan.shtohrinets@emodoinc.com>
2024-03-10 20:07:20 +0200

    add app layout

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -2,25 +2,31 @@ import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
 import Cost from '../pages/Cost';
 import Speed from '../pages/Speed';
 import Error from '../pages/Error';
+import { AppLayout } from '../layouts/AppLayout';
 
 const baseUrl = process.env.NODE_ENV === 'production' ? '/equipment-cost-calculator-app' : '';
 
 const router = createBrowserRouter([
   {
     path: baseUrl,
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: `/cost`,
+        element: <Cost />,
+        errorElement: <Error />,
+      },
+      {
+        path: `/speed`,
+        element: <Speed />,
+        errorElement: <Error />,
+      },
+    ],
+  },
+  {
+    path: '*',
     element: <Navigate to={`${baseUrl}/cost`} />,
-    errorElement: <Error />,
-    children: [],
-  },
-  {
-    path: `${baseUrl}/cost`,
-    element: <Cost />,
-    errorElement: <Error />,
-  },
-  {
-    path: `${baseUrl}/speed`,
-    element: <Speed />,
-    errorElement: <Error />,
   },
 ]);
 
---
+++ src/layouts/AppLayout/index.tsx
@@ -0,0 +1,21 @@
+import { NavLink, Outlet } from 'react-router-dom';
+import styles from './Header.module.css';
+
+export function AppLayout() {
+  return (
+    <>
+      <header className={styles.headerContainer}>
+        <NavLink to={'/cost'} className={({ isActive }) => (isActive ? styles.activeLink : styles.link)}>
+          Програма обчислення витрат
+        </NavLink>
+        <NavLink to={'/speed'} className={({ isActive }) => (isActive ? styles.activeLink : styles.link)}>
+          Програма обчислення швидкодії
+        </NavLink>
+      </header>
+
+      <main className={styles.mainContainer}>
+        <Outlet />
+      </main>
+    </>
+  );
+}
